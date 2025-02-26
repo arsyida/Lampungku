ccfdf97bdd88d9af8680557fd43ecca507c0e3b8
arsyida <arsyida08@gmail.com>
2025-02-26 15:47:53 +0700

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="<?= base_url('css/tailwind-output.css'); ?>" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-4xl font-bold underline text-center mt-20 text-amber-400">
+    Hello world!
+  </h1>
+</body>
+</html>
